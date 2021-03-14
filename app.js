5af9a3adaa456ddceaa6dc6df566fec3177d3296
vishnugopy <vishnugoby0010@gmail.com>
2021-03-14 21:06:11 +0100

    :tada: Added html css js basics

---
+++ app.js
@@ -0,0 +1,22 @@
+let namevalue = document.querySelector("#name");
+let job = document.querySelector("#job");
+let mail = document.querySelector("#email");
+let tel = document.querySelector("#phonenumber");
+let logo = document.querySelector("#logo");
+let generatebutton = document.querySelector("#generatebutton");
+
+
+let namepreview = document.querySelector("#namepreview");
+let jobpreview = document.querySelector("#jobpreview");
+let telpreview = document.querySelector("#telpreview");
+let mailpreview = document.querySelector("#mailpreview");
+let logopreview = document.querySelector("#logopreview");
+
+
+generatebutton.addEventListener("click" , () =>{
+    namepreview.innerHTML = namevalue.value ;
+    jobpreview.innerHTML = job.value ;
+    mailpreview.innerHTML = mail.value ;
+    telpreview.innerHTML = tel.value ;
+    logopreview.src = logo;
+})
